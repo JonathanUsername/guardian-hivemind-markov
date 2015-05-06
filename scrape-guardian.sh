08bfd0f8898595f5ede8d64f0559d3b4c050f30d
Jonathan <jonathan.j.king@gmail.com>
2015-05-06 12:37:11 +0100

    Rearranged directories, added json library

--- scrape-guardian.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-echo "Simple guardian scraper - first parameter is your API key, second is the search query, ie. guardian-scraper hgu54hng5n 'David Cameron'"
-curl "http://content.guardianapis.com/search?show-fields=body&api-key=$1&q=$2" | jq .response.results[].fields.body | sed 's/<[^>]\+>/ /g'
