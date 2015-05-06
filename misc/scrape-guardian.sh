be56d0f19242d245f11a52b7a5bf556d6cd8b32c
Jonathan <jonathan.j.king@gmail.com>
2015-05-06 13:58:28 +0100

    Fixed issue with spaces

--- misc/scrape-guardian.sh
+++ misc/scrape-guardian.sh
@@ -2,4 +2,5 @@
 
 # echo "Simple guardian scraper - first parameter is your API key, second is the search query, ie. guardian-scraper hgu54hng5n 'David Cameron'"
 
-curl -s "http://content.guardianapis.com/search?show-fields=body&page-size=200&api-key=$1&q=$2" | jq .response.results[].fields.body | sed 's/<[^>]\+>/ /g'
+QUERY=$(echo "$2" | sed s/\ /\+/g)
+curl -s "http://content.guardianapis.com/search?show-fields=body&page-size=200&api-key=$1&q=$QUERY" | jq .response.results[].fields.body | sed 's/<[^>]\+>/ /g'
